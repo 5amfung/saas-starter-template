7bf36ef9b8d37812fcc294ab5ddba154af62794b
Sam Fung <sam.fung@gmail.com>
2026-02-07 23:13:24 -0800

    chore: add eslint plugin for router

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,10 @@
 //  @ts-check
 
 import { tanstackConfig } from '@tanstack/eslint-config';
+import pluginRouter from '@tanstack/eslint-plugin-router';
 
 export default [
   { ignores: ['eslint.config.js', 'prettier.config.js'] },
   ...tanstackConfig,
+  ...pluginRouter.configs['flat/recommended'],
 ];
