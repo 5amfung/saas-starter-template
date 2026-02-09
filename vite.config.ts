d7b6e547eec1503a9e519a213523c626175297f2
Sam Fung <sam.fung@gmail.com>
2026-02-09 01:39:38 -0800

    chore: update TanStack Router configuration in Vite setup

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import viteReact from '@vitejs/plugin-react';
 import viteTsConfigPaths from 'vite-tsconfig-paths';
 import tailwindcss from '@tailwindcss/vite';
 import { nitro } from 'nitro/vite';
-import { tanstackRouter } from '@tanstack/router-plugin/vite';
 
 const config = defineConfig({
   plugins: [
@@ -16,10 +15,11 @@ const config = defineConfig({
       projects: ['./tsconfig.json'],
     }),
     tailwindcss(),
-    tanstackStart(),
-    tanstackRouter({
-      semicolons: true,
-    }), // Must be before @vitejs/plugin-react
+    tanstackStart({
+      router: {
+        semicolons: true
+      }
+    }),
     viteReact(),
   ],
 });
