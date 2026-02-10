f9ed335598c1b627548a33d037f39273263f1064
Sam Fung <sam.fung@gmail.com>
2026-02-09 17:26:33 -0800

    chore: enhance ESLint configuration with import/order rules and remove unused Prettier plugin

--- eslint.config.js
+++ eslint.config.js
@@ -7,4 +7,33 @@ export default [
   { ignores: ['eslint.config.js', 'prettier.config.js'] },
   ...tanstackConfig,
   ...pluginRouter.configs['flat/recommended'],
+  {
+    rules: {
+      'import/order': [
+        'error',
+        {
+          groups: [
+            'builtin',
+            'external',
+            'internal',
+            ['parent', 'sibling', 'index'],
+          ],
+          pathGroups: [
+            {
+              pattern: 'react',
+              group: 'external',
+              position: 'before',
+            },
+            {
+              pattern: '@/**',
+              group: 'internal',
+              position: 'before',
+            },
+          ],
+          pathGroupsExcludedImportTypes: ['react'],
+          'newlines-between': 'never',
+        },
+      ],
+    },
+  },
 ];
