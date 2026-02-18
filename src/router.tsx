0aa82c036521a6afdb08d6786b95738f9deb7a72
Sam Fung <sam.fung@gmail.com>
2026-02-18 01:46:27 -0800

    feat(query): add query sync across browser tabs or windows

--- src/router.tsx
+++ src/router.tsx
@@ -1,10 +1,17 @@
 import { QueryClient } from '@tanstack/react-query';
 import { createRouter } from '@tanstack/react-router';
 import { setupRouterSsrQueryIntegration } from '@tanstack/react-router-ssr-query';
+import { broadcastQueryClient } from '@tanstack/query-broadcast-client-experimental';
 import { routeTree } from './routeTree.gen';
+
 
 export const getRouter = () => {
   const queryClient = new QueryClient();
+
+  broadcastQueryClient({
+    queryClient,
+    broadcastChannel: 'query-sync',
+  });
 
   const router = createRouter({
     routeTree,
