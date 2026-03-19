6d28f25f959c015bc7f1e2f6dd9d07e4573935e5
Sam Fung <sam.fung@gmail.com>
2026-03-19 11:09:09 -0700

    chore: simplify eslint ignores in root config
    - Removed 'node_modules/**' and '.output/**' from the ignores array in eslint.config.js for a cleaner configuration.

--- eslint.config.js
+++ eslint.config.js
@@ -4,14 +4,7 @@ import config from '@workspace/eslint-config/base';
 
 export default [
   {
-    ignores: [
-      'apps/**',
-      'packages/**',
-      'node_modules/**',
-      '.output/**',
-      '.turbo/**',
-      'eslint.config.js',
-    ],
+    ignores: ['apps/**', 'packages/**', '.turbo/**', 'eslint.config.js'],
   },
   ...config,
 ];
